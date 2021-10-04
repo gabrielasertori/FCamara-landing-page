d6f9c201fee2fde196e3483d3a87f178dbd19de8
gabriela <gabriela@sertori.com.br>
2021-10-04 17:56:08 -0300

    mobile menu done with js

--- src/script.js
+++ src/script.js
@@ -1,14 +1,17 @@
 'use strict'
 
 const	menu = document.getElementById("menu");
+const	menuContent = document.getElementById("menu-content");
 let		menuOpen = false;
 
 menu.addEventListener('click', () => {
 	if (!menuOpen) {
 		menuOpen = true;
 		menu.classList.add("open");
+		menuContent.classList.add("open");
 	} else {
 		menuOpen = false;
 		menu.classList.remove("open");
+		menuContent.classList.remove("open");
 	}
 })
