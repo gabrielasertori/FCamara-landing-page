30a62eea8f79c1e1472e6443590ab235cd1f266f
gabriela <gabriela@sertori.com.br>
2021-10-04 22:58:05 -0300

    finish linking

--- src/script.js
+++ src/script.js
@@ -14,4 +14,14 @@ menu.addEventListener('click', () => {
 		menu.classList.remove("open");
 		menuContent.classList.remove("open");
 	}
-})
+
+	const	menuList = document.querySelectorAll(".menu-content-list-item");
+	let		i;
+
+	menuList.forEach(i => {
+		i.addEventListener('click', () => {
+			menuOpen = false;
+			menu.classList.remove("open");
+			menuContent.classList.remove("open");
+		})
+})})
